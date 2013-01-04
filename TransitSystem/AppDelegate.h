47d77ea00e6e8ed2e13ed5fab974c55a2c7727b2
li haijin <lihaijinlinux@gmail.com>
2013-01-04 11:49:20 +0800

    Initial Commit

---
+++ TransitSystem/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TransitSystem
+//
+//  Created by li haijin on 13-1-4.
+//  Copyright (c) 2013年 li haijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TransitSystem/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TransitSystem
+//
+//  Created by li haijin on 13-1-4.
+//  Copyright (c) 2013年 li haijin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
